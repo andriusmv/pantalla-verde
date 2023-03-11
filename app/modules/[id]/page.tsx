76dee1b836a9bf0bd0a8ea2da4e9a04508b07cc0
Andres Moreno <99091918+andriusmv@users.noreply.github.com>
2023-03-11 19:10:54 +0100

    mapping problems

--- app/modules/[id]/page.tsx
+++ app/modules/[id]/page.tsx
@@ -2,18 +2,18 @@ import { notFound } from 'next/navigation';
 import supabase from '../../../utils/supabase';
 import Player from '../../player';
 import styles from './/page.module.css';
+import React from "react";
+import Link from "next/link";
 
 export async function generateStaticParams() {
     const { data: module } = await supabase.from("module").select("id");
-    const { data: moduleId } = await supabase.from("module").select().in('course_id', ['blender']);
 
-  
     return module?.map(({ id }) => ({
       id,
     }));
   }
   
-  export default async function module({
+  export default async function Module({
     params: { id },
   }: {
     params: { id: string };
@@ -22,34 +22,22 @@ export async function generateStaticParams() {
       .from("module")
       .select()
       .match({ id })
-      .single();
+      .select();
   
     if (!module) {
       notFound();
     }
   
     return (
-        <div className={styles.sidebar}>
-  <aside className={styles.sidebar__sidebar}>
-    
-      <ul>
-        <li>
-          <a href='/modules/toursvirtuales'>Tours Virtuales</a>
-        </li>
-        <li>
-          <a href='/modules/integracion'>Integracion</a>
-        </li>
-      </ul>
-    
-  </aside>
-  <main className={styles.sidebar__main}>
-  <div className={styles.playerWrapper}>
+  <>
+  {module.map((module) => (
+  <div key={module.id} className={styles.playerWrapper}>
     <Player
-      playbackId="oKthiMVIMEKXJZcojBlvzI55002gsIWEwYH00mBCP5Zao"
+      playbackId={module.video_url}
       placeholder="fO0101O00KgKEVbWLe2tY552HMcQ02XmnuvFvOgAIvCStQI"
       aspectRatio={"16/9"} />
     </div>
-  </main>
-  </div>
+        ))}
+  </>
     );
 }
