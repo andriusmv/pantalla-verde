c24690e0d49b9a80ab833886045a12e9803a2878
Andres Moreno <99091918+andriusmv@users.noreply.github.com>
2023-06-24 18:54:25 +0200

    matrix removed

--- app/error.tsx
+++ app/error.tsx
@@ -1,14 +1,7 @@
 "use client";
 
-import './globals.css'
-import MatrixRainV12 from './matrix/page';
-
-export default function Error() { 
-    
-    return (
-        <div>
-          <h2>Sucedió un error inesperado!</h2>
-          <MatrixRainV12 />
-        </div>
-      );
-    }
+export default function Error() {
+    return(
+        <p>Sucedió algo inesperado.</p>
+    );
+}
